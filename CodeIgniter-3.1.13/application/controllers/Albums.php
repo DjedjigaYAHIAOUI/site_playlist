af5b32b662aa59081e8e01cba1dedf8656e8d0a3
Yahiaoui <djedjiga.yahiaoui@etu-u-pec.fr>
2024-05-30 09:56:42 +0200

    m

--- CodeIgniter-3.1.13/application/controllers/Albums.php
+++ CodeIgniter-3.1.13/application/controllers/Albums.php
@@ -3,36 +3,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Albums extends CI_Controller {
 
-    public function __construct(){
-        parent::__construct();
-        $this->load->model('model_music');
-    }
-
-    public function index(){
-        $albums = $this->model_music->getAlbums();
-        $this->load->view('layout/header');
-        $this->load->view('albums_list',['albums'=>$albums]);
-        $this->load->view('layout/footer');
-    }
-
-    public function view($albumId) {
-      
-        $album = $this->model_music->getAlbumById($albumId);
-
-       
-        if ($album) {
-           
-            $songs = $this->model_music->getSongsByAlbumId($albumId);
-
-           
-            $this->load->view('layout/header');
-            $this->load->view('album_song', ['album' => $album, 'songs' => $songs]); // Vue renommée ici
-            $this->load->view('layout/footer');
-        } else {
-            
-            show_error('Album not found.', 404);
-        }
-    }
+public function __construct(){
+parent::__construct();
+$this->load->model('model_music');
+}
+public function index(){
+$albums = $this->model_music->getAlbums();
+$this->load->view('layout/header');
+$this->load->view('albums_list',['albums'=>$albums]);
+$this->load->view('layout/footer');
+}
 
 }
-?>
+

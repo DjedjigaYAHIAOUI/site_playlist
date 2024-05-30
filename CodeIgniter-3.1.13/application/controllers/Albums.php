f0b09748c61d326484ef583f34fd10119a1f8131
Yahiaoui <djedjiga.yahiaoui@etu-u-pec.fr>
2024-05-30 11:17:29 +0200

    chansons

--- CodeIgniter-3.1.13/application/controllers/Albums.php
+++ CodeIgniter-3.1.13/application/controllers/Albums.php
@@ -3,15 +3,29 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Albums extends CI_Controller {
 
-public function __construct(){
-parent::__construct();
-$this->load->model('model_music');
-}
-public function index(){
-$albums = $this->model_music->getAlbums();
-$this->load->view('layout/header');
-$this->load->view('albums_list',['albums'=>$albums]);
-$this->load->view('layout/footer');
-}
+    public function __construct(){
+        parent::__construct();
+        $this->load->model('model_music');
+    }
 
+    public function index(){
+        $albums = $this->model_music->getAlbums();
+        $this->load->view('layout/header');
+        $this->load->view('albums_list', ['albums' => $albums]);
+        $this->load->view('layout/footer');
+    }
+
+    public function view($albumId) {
+        $album = $this->model_music->getAlbumById($albumId);
+
+        if ($album) {
+            $songs = $this->model_music->getSongsByAlbumId($albumId);
+            $this->load->view('layout/header');
+            $this->load->view('album_song', ['album' => $album, 'songs' => $songs]);
+            $this->load->view('layout/footer');
+        } else {
+            show_error('Album not found.', 404);
+        }
+    }
 }
+?>
--- CodeIgniter-3.1.13/application/models/Model_music.php
+++ CodeIgniter-3.1.13/application/models/Model_music.php
@@ -1,23 +1,21 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');                                             
 
 class Model_music extends CI_Model {
-public function __construct(){
-$this->load->database();
-}
+    public function __construct(){
+        $this->load->database();
+    }
 
-public function getAlbums(){
-$query = $this->db->query(
-"SELECT album.name,album.id,year,artist.name as artistName, genre.name as genreName,jpeg 
-FROM album 
-JOIN artist ON album.artistid = artist.id
-JOIN genre ON genre.id = album.genreid
-JOIN cover ON cover.id = album.coverid
-ORDER BY year
-"
-);
-return $query->result();
-}
-
+    public function getAlbums(){
+        $query = $this->db->query(
+            "SELECT album.name, album.id, year, artist.name as artistName, genre.name as genreName, jpeg 
+            FROM album 
+            JOIN artist ON album.artistid = artist.id
+            JOIN genre ON genre.id = album.genreid
+            JOIN cover ON cover.id = album.coverid
+            ORDER BY year"
+        );
+        return $query->result();
+    }
 
     public function getArtists(){
         $query = $this->db->query(
@@ -26,5 +24,27 @@ return $query->result();
         return $query->result();
     }
 
+    public function getAlbumById($albumId) {
+        $query = $this->db->query(
+            "SELECT album.name, album.id, year, artist.name as artistName, genre.name as genreName, jpeg 
+            FROM album 
+            JOIN artist ON album.artistid = artist.id
+            JOIN genre ON genre.id = album.genreid
+            JOIN cover ON cover.id = album.coverid
+            WHERE album.id = ?", array($albumId)
+        );
+        return $query->row();
+    }
 
+    public function getSongsByAlbumId($albumId) {
+        $query = $this->db->query(
+            "SELECT track.songId as id, song.name, track.number, track.duration
+            FROM track
+            JOIN song ON track.songId = song.id
+            WHERE track.albumId = ?
+            ORDER BY track.number", array($albumId)
+        );
+        return $query->result();
+    }
 }
+?>

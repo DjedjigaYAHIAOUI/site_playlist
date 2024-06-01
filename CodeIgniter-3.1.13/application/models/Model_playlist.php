e5caaac0984a9da49b50092ee5d8b7a73f1ebed1
hafsati marwa <marwa.hafsati@etu.u-pec.fr>
2024-06-01 20:18:06 +0200

    model

---
+++ CodeIgniter-3.1.13/application/models/Model_playlist.php
@@ -0,0 +1,26 @@
+// application/models/Model_playlist.php
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Model_playlist extends CI_Model {
+
+    public function __construct(){
+        parent::__construct();
+        $this->load->database();
+    }
+
+    public function getPlaylistsByUserId($utilisateur_id) {
+        $this->db->where('utilisateur_id', $utilisateur_id);
+        $query = $this->db->get('playlists');
+        return $query->result();
+    }
+
+    public function createPlaylist($utilisateur_id, $nom) {
+        $data = array(
+            'utilisateur_id' => $utilisateur_id,
+            'nom' => $nom
+        );
+        return $this->db->insert('playlists', $data);
+    }
+}
+?>

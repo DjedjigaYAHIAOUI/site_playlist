498ad7712f03909bd9dc1423e6666eb3ed297756
hafsati marwa <marwa.hafsati@etu.u-pec.fr>
2024-06-02 14:55:10 +0200

    l

--- CodeIgniter-3.1.13/application/controllers/Auth.php
+++ CodeIgniter-3.1.13/application/controllers/Auth.php
@@ -14,7 +14,39 @@ class Playlist extends CI_Controller {
             redirect('auth/login');
         }
     }
+    public function register() {
+        if ($this->input->post()) {
+            $nom_utilisateur = $this->input->post('nom_utilisateur');
+            $mot_de_passe = $this->input->post('mot_de_passe');
+            $user_id = $this->model_user->register($nom_utilisateur, $mot_de_passe);
+            if ($user_id) {
+                // Inscription réussie, rediriger vers la page de création de playlist
+                $this->session->set_userdata('utilisateur_id', $user_id);
+                redirect('playlist/create');
+            } else {
+                // Gérer l'échec de l'inscription
+                $this->load->view('register', array('error' => 'Échec de l\'inscription. Veuillez réessayer.'));
+            }
+        } else {
+            $this->load->view('register');
+        }
+    }
 
+    public function login() {
+        if ($this->input->post()) {
+            $nom_utilisateur = $this->input->post('nom_utilisateur');
+            $mot_de_passe = $this->input->post('mot_de_passe');
+            $user = $this->model_user->login($nom_utilisateur, $mot_de_passe);
+            if ($user) {
+                $this->session->set_userdata('utilisateur_id', $user->id);
+                redirect('playlist');
+            } else {
+                $this->load->view('login', array('error' => 'Nom d\'utilisateur ou mot de passe invalide.'));
+            }
+        } else {
+            $this->load->view('login');
+        }
+    }
     public function index() {
         $utilisateur_id = $this->session->userdata('utilisateur_id');
         $playlists = $this->Model_playlist->getPlaylistsByUserId($utilisateur_id);

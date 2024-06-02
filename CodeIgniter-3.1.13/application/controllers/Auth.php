d429b7c8a8be90ca95f697a40f9570e1f6be79b3
hafsati marwa <marwa.hafsati@etu.u-pec.fr>
2024-06-02 17:03:47 +0200

    sa marche pas

--- CodeIgniter-3.1.13/application/controllers/Auth.php
+++ CodeIgniter-3.1.13/application/controllers/Auth.php
@@ -16,11 +16,9 @@ class Auth extends CI_Controller {
             $mot_de_passe = $this->input->post('mot_de_passe');
             $user_id = $this->Model_user->register($nom_utilisateur, $mot_de_passe);
             if ($user_id) {
-                // Inscription réussie, rediriger vers la page de création de playlist
                 $this->session->set_userdata('utilisateur_id', $user_id);
-                redirect('playlist/create');
+                redirect('playlist/create'); // Redirection après inscription
             } else {
-                // Gérer l'échec de l'inscription
                 $this->load->view('register', array('error' => 'Échec de l\'inscription. Veuillez réessayer.'));
             }
         } else {
@@ -35,7 +33,7 @@ class Auth extends CI_Controller {
             $user = $this->Model_user->login($nom_utilisateur, $mot_de_passe);
             if ($user) {
                 $this->session->set_userdata('utilisateur_id', $user->id);
-                redirect('playlist');
+                redirect('playlist/create'); // Redirection après connexion
             } else {
                 $this->load->view('login', array('error' => 'Nom d\'utilisateur ou mot de passe invalide.'));
             }

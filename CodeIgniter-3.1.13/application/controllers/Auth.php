04e4ca4c1bfe86bfc6ce0fbc4f29a29059717258
hafsati marwa <marwa.hafsati@etu.u-pec.fr>
2024-06-02 14:58:29 +0200

    l

--- CodeIgniter-3.1.13/application/controllers/Auth.php
+++ CodeIgniter-3.1.13/application/controllers/Auth.php
@@ -1,24 +1,20 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Playlist extends CI_Controller {
+class Auth extends CI_Controller {
 
     public function __construct() {
         parent::__construct();
-        $this->load->model('Model_playlist');
         $this->load->model('Model_user');
         $this->load->library('session');
-
-        // Vérifier l'authentification de l'utilisateur ici
-        if (!$this->session->userdata('utilisateur_id')) {
-            redirect('auth/login');
-        }
+        $this->load->helper('url');
     }
+
     public function register() {
         if ($this->input->post()) {
             $nom_utilisateur = $this->input->post('nom_utilisateur');
             $mot_de_passe = $this->input->post('mot_de_passe');
-            $user_id = $this->model_user->register($nom_utilisateur, $mot_de_passe);
+            $user_id = $this->Model_user->register($nom_utilisateur, $mot_de_passe);
             if ($user_id) {
                 // Inscription réussie, rediriger vers la page de création de playlist
                 $this->session->set_userdata('utilisateur_id', $user_id);
@@ -36,7 +32,7 @@ class Playlist extends CI_Controller {
         if ($this->input->post()) {
             $nom_utilisateur = $this->input->post('nom_utilisateur');
             $mot_de_passe = $this->input->post('mot_de_passe');
-            $user = $this->model_user->login($nom_utilisateur, $mot_de_passe);
+            $user = $this->Model_user->login($nom_utilisateur, $mot_de_passe);
             if ($user) {
                 $this->session->set_userdata('utilisateur_id', $user->id);
                 redirect('playlist');
@@ -47,25 +43,10 @@ class Playlist extends CI_Controller {
             $this->load->view('login');
         }
     }
-    public function index() {
-        $utilisateur_id = $this->session->userdata('utilisateur_id');
-        $playlists = $this->Model_playlist->getPlaylistsByUserId($utilisateur_id);
-        $this->load->view('layout/header');
-        $this->load->view('playlist_list', ['playlists' => $playlists]);
-        $this->load->view('layout/footer');
-    }
 
-    public function create() {
-        $utilisateur_id = $this->session->userdata('utilisateur_id');
-        if ($this->input->post()) {
-            $nom = $this->input->post('nom');
-            $this->Model_playlist->createPlaylist($utilisateur_id, $nom);
-            redirect('playlist');
-        } else {
-            $this->load->view('layout/header');
-            $this->load->view('create_playlist');
-            $this->load->view('layout/footer');
-        }
+    public function logout() {
+        $this->session->unset_userdata('utilisateur_id');
+        redirect('auth/login');
     }
 }
 ?>

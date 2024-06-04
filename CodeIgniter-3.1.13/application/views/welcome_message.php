c56572671d45b946639e8cf1f2c701586b28013a
Yahiaoui <djedjiga.yahiaoui@etu-u-pec.fr>
2024-06-04 16:16:00 +0200

    .

--- CodeIgniter-3.1.13/application/views/welcome_message.php
+++ CodeIgniter-3.1.13/application/views/welcome_message.php
@@ -10,18 +10,3 @@
     </form>
 </div>
 
-<h2>Liste des artistes</h2>
-<ul>
-    <?php foreach ($artists as $artist): ?>
-        <li><?php echo $artist->name; ?></li>
-    <?php endforeach; ?>
-</ul>
-
-<h2>Liste des albums</h2>
-<ul>
-    <?php foreach ($albums as $album): ?>
-        <li><?php echo $album->name; ?></li>
-    <?php endforeach; ?>
-</ul>
-
-<?php $this->load->view('layout/footer'); ?>
